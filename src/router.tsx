664df73b2975328a41804a4ec89f3529649751b1
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-11-21 19:52:26 -0500

    Routes set up

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes >
-                
+                <Route path="/" element={<IndexPage />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+
+                <Route path='*' element={<Navigate to='/' />} />
             </Routes>
         </BrowserRouter>
     )
