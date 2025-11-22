f3c7b1eed974eb6f41f6077c3e88d041d09bd713
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-11-21 21:32:40 -0500

    Search drink form created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function () {
+
+    const { pathname } = useLocation()
+
+    const isHome = useMemo(() => pathname === '/', [pathname])
 
     return (
         <header className="bg-slate-800">
@@ -31,6 +36,50 @@ export default function () {
                         </NavLink>
                     </nav>
                 </div>
+
+                {isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >
+                                Name or Ingredients:
+                            </label>
+
+                            <input
+                                type="text"
+                                id="ingredient"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                                placeholder="Drink name or Ingredient. e.g., Vodka, Tequila, Coffee"
+                            />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="category"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >
+                                Category:
+                            </label>
+
+                            <select
+                                id="ingredient"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg bg-white"
+                            >
+                                <option value="">-- Select an Option --</option>
+                            </select>
+                        </div>
+
+                        <input
+                            type="submit"
+                            value="Search Recipes"
+                            className="cursor-pointer bg-orange-800 hover:bg-orange-900 active:bg-orange-950 text-white font-extrabold w-full p-3 rounded-lg uppercase transition-colors duration-300"
+                        />
+                    </form>
+                )}
             </div>
         </header>
     )
