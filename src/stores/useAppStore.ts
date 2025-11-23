5022c9e69cc35c5536669dd4bb857d0e71a23087
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-11-23 11:03:11 -0500

    Favorites slices set up

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice"
+import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice"
 
-
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+// Slice Pattern
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a)
 })))
