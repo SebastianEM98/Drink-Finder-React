03f3c1494381e65ef4aa5aa085e2a1a5ef04db9b
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-11-23 13:11:32 -0500

    Improving performance

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,25 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import IndexPage from "./pages/IndexPage"
-import FavoritesPage from "./pages/FavoritesPage"
 import Layout from "./layouts/Layout"
+const IndexPage = lazy(() => import("./pages/IndexPage"))
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"))
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes >
                 <Route element={<Layout />}>
-                    <Route path="/" element={<IndexPage />} />
-                    <Route path="/favorites" element={<FavoritesPage />} />
+                    <Route path="/" element={
+                        <Suspense fallback="Loading...">
+                            <IndexPage />
+                        </Suspense>
+                    } />
+
+                    <Route path="/favorites" element={
+                        <Suspense fallback="Loading...">
+                            <FavoritesPage />
+                        </Suspense>
+                    } />
                 </Route>
 
                 <Route path='*' element={<Navigate to='/' />} />
